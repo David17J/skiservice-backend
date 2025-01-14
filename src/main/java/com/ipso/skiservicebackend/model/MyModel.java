4c8fb36114c6f1c8338adaefa46bf3f654353c52
David L <david.lindoerfer@student.ipso.ch>
2025-01-14 20:02:00 +0100

    backup #1

--- src/main/java/com/ipso/skiservicebackend/model/MyModel.java
+++ src/main/java/com/ipso/skiservicebackend/model/MyModel.java
@@ -1,7 +1,8 @@
 package com.ipso.skiservicebackend.model;
 
+
 public class MyModel {
-   private String property1;
-   private String property2;
-   // Getter und Setter Methoden
+    public String property1;
+    public String property2;
+    // Getter und Setter Methoden
 }
